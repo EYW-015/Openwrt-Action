01623b1075c211f5357158f81de165a82b075d94
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2020-10-16 21:41:03 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#替换添加老竭力Argon主题===========
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+popd
+#添加openclash===========
+mkdir package/luci-app-openclash
+pushd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+popd
