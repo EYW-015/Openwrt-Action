8c8067f356bd8ddf5553c5c0eed61612977a0f1d
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2020-10-21 18:40:56 +0900

    Create diy-part2-64.sh

---
+++ diy-part2-64.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#替换添加老竭力Argon主题===========
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+popd
+#添加openclash===========
+mkdir package/luci-app-openclash
+pushd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+popd
