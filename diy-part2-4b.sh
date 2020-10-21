ef98dda84ed9f7542b4543f589b56a18cac5274e
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2020-10-21 18:42:12 +0900

    Update diy-part2-4b.sh

--- diy-part2-4b.sh
+++ diy-part2-4b.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.60.1/g' package/base-files/files/bin/config_generate
 #替换添加老竭力Argon主题===========
 pushd package/lean
 rm -rf luci-theme-argon
