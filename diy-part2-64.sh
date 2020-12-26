4201767ebd7edc3bf78008f3abf870b4a783b4eb
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2020-12-26 14:20:57 +0900

    Update diy-part2-64.sh

--- diy-part2-64.sh
+++ diy-part2-64.sh
@@ -12,13 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '112a set network.$1.gateway='192.168.50.1'' package/base-files/files/bin/config_generate
-sed -i '112a set network.$1.dns='223.5.5.5'' package/base-files/files/bin/config_generate
-sed -i '113s/^/\t\t\t\t/g' package/base-files/files/bin/config_generate
-sed -i '114s/^/\t\t\t\t/g' package/base-files/files/bin/config_generate
-#降低qBit版本===========
-sed -i 's/4.3.1/4.3.0.1/g' package/lean/qBittorrent/Makefile
-sed -i 's/4428ed710cfbcc48e75c4cdb014b803ea3f67311f55f4cba0d148987986bd6c8/af17daa57d750522357393aca32a4dd57f5b0f271f2c49b3ce6908baee5ed93b/g' package/lean/qBittorrent/Makefile
+sed -i '/set network.$1.netmask='$netm'/a \\t\t\t\tset network.$1.gateway='192.168.50.1'' package/base-files/files/bin/config_generate
+#降低qBit版本4.2.5===========
+sed -i '4c PKG_VERSION:=4.2.5' package/lean/qBittorrent/Makefile
+sed -i '9c PKG_HASH:=1dac52d6fe4b0c44dba04fcfc41f519c57a69cb30580255edca95c87053a4324' package/lean/qBittorrent/Makefile
 #替换添加老竭力Argon主题===========
 pushd package/lean
 rm -rf luci-theme-argon
