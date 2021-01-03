9eb959e255f4f538066515f49fe566bc0bf22a46
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2021-01-04 00:01:29 +0900

    Modify IP & Host name & qBit version & Luci Theme

--- diy-part2-4b.sh
+++ diy-part2-4b.sh
@@ -26,13 +26,3 @@ pushd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 popd
-#添加openclash===========
-mkdir package/luci-app-openclash
-pushd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-popd
