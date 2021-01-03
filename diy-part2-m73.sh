43e9f847f162089f7cf7ac93c53bff3aa19772ba
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2021-01-03 23:54:34 +0900

    Update diy-part2-m73.sh

--- diy-part2-m73.sh
+++ diy-part2-m73.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#网关=192.168.50.1/DNS=192.168.50.1
 sed -i "/set network.\$1.netmask='\$netm'/a \\\t\t\t\tset network.\$1.gateway='192.168.50.1'\n\t\t\t\tset network.\$1.dns='192.168.50.1'" package/base-files/files/bin/config_generate
 #修改主机名称密码
 sed -i 's/OpenWrt/M73/g' package/base-files/files/bin/config_generate
@@ -22,14 +23,4 @@ sed -i '9c PKG_HASH:=1dac52d6fe4b0c44dba04fcfc41f519c57a69cb30580255edca95c87053
 pushd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-popd
-#添加openclash===========
-mkdir package/luci-app-openclash
-pushd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
 popd
