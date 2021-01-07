b5276062297990c30bfa1c8e80a27e9bf1d262dd
evangelion015 <36738889+evangelion015@users.noreply.github.com>
2021-01-07 23:41:03 +0900

    No changed

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #添加openclash===========
-sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
