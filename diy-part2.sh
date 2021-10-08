ee2ee15ff6e71684f53a835103a528abd2841a48
EYW-015 <rk14580@163.com>
2021-10-08 17:45:43 +0900

    update 1008

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,26 +11,28 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.60.1/g' package/base-files/files/bin/config_generate
 
+sed -i 's/192.168.1.1/192.168.60.1/g' package/base-files/files/bin/config_generate
 # Change Hostname
 #sed -i 's/OpenWrt/OP-EYW/g' package/base-files/files/bin/config_generate
 
-# Version of qBittorrent
+# Set Gateway & DNS
+#sed -i "/set network.\$1.netmask='\$netm'/a \\\t\t\t\tset network.\$1.gateway='192.168.50.1'\n\t\t\t\tset network.\$1.dns='192.168.50.1'" package/base-files/files/bin/config_generate
+
+# Version of qBittorren
 #sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.1.9' package/lean/qBittorrent/Makefile
 #sed -i '/PKG_HASH:=/c PKG_HASH:=620127f73f88ed3f2b2e4195cbc641f7c27f967d3b045e45f7916c0995fd61fe' package/lean/qBittorrent/Makefile
 
 #sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.2.5' package/lean/qBittorrent/Makefile
 #sed -i '/PKG_HASH:=/c PKG_HASH:=1dac52d6fe4b0c44dba04fcfc41f519c57a69cb30580255edca95c87053a4324' package/lean/qBittorrent/Makefile
 
-#sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.4.1' package/lean/qBittorrent/Makefile
-#sed -i '/PKG_HASH:=/c PKG_HASH:=8f615aa06f77286881d40b5f769f648221128b46f0bffc1f2fa101068f99ebe8' package/lean/qBittorrent/Makefile
+sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.7_v1.2.14' package/lean/qBittorrent-static/Makefile
+sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.7' package/lean/qBittorrent/Makefile
+sed -i '/PKG_HASH:=/c PKG_HASH:=d17c0bd852aaf8b75d61026ee213ad9147c37d8e3a14a3137b735732061bd1b1' package/lean/qBittorrent/Makefile
 
-#sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.5' package/lean/qBittorrent/Makefile
-#sed -i '/PKG_HASH:=/c PKG_HASH:=83b6165f9f969406a2daabc7f49bbdc79aa6840c1e707cbade97eeb15a4907f3' package/lean/qBittorrent/Makefile
-
-#sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.6' package/lean/qBittorrent/Makefile
-#sed -i '/PKG_HASH:=/c PKG_HASH:=dc673e20e69077dcebd836dade38e15c19289a8cda5837bf1f19206f16bf4751' package/lean/qBittorrent/Makefile
+#sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.8_v1.2.14' package/lean/qBittorrent-static/Makefile
+#sed -i '/PKG_VERSION:=/c PKG_VERSION:=4.3.8' package/lean/qBittorrent/Makefile
+#sed -i '/PKG_HASH:=/c PKG_HASH:=cc11f797dd146e6aac8feee8feffb1e429d61551f48b577d32b2239ec5e72ccb' package/lean/qBittorrent/Makefile
 
 #Custom package
 rm -rf package/lean/luci-theme-argon
